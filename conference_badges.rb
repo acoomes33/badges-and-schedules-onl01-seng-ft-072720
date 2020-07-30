3647698f3d68e21a173942807cf08d5de0aba7ea
Alan Coomes <acoomes1@cord.edu>
2020-07-30 17:15:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+ 
+end
+
+def batch_badge_creator(attendees_array)
+  badge_array = []
+  attendees_array.each do |attendee|
+    badge = "Hello, my name is #{attendee}."
+    badge_array.push(badge)
+  end
+  badge_array
+end
+  
+def assign_rooms
+  welcome_array = []
+  attendees_array.each do |speaker|
+    room_num = speaker.index + 1
+   greeting = "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    welcome_array.push(greeting)
+  end
+    welcome_array
+end
+
+def printer
+  
